26e963b519d6a6a2db3a6aadb39175da28a0f5cd
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2020-10-27 17:43:45 +0100

    Fonction choix init 1

--- choix_init.c
+++ choix_init.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #define tailleMatrice 10
 
-void choix_init() {
+void choix_init(int matrice [][tailleMatrice]) {
 
-   int choix;
+   int choix = 0;
+   
    while((choix<1)||(choix>3)){
-      printf("Choisissez un type d'initialisation (1 pour canon, 2 pour clignotants et 3 pour hasard");
+      printf("Choisissez un type d'initialisation (1 pour canon, 2 pour clignotants et 3 pour hasard \n");
       scanf("%d", &choix);
    }
+   if(choix==1)
+      init_canon(matrice);
+   if (choix==2)
+      init_clignotants(matrice);
+   if (choix==3)
+      init_random(matrice);
 
 }
 
-int main( ) {
 
-   choix_init();
-   return 0;
-}
