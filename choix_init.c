1c189f196bd8132878fac865b7670f77acab1503
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2020-10-27 18:17:10 +0100

    Modification choix 1

--- choix_init.c
+++ choix_init.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define tailleMatrice 10
 
-void choix_init(int matrice [][tailleMatrice]) {
+int choix_init() {
 
    int choix = 0;
    
@@ -10,13 +9,9 @@ void choix_init(int matrice [][tailleMatrice]) {
       printf("Choisissez un type d'initialisation (1 pour canon, 2 pour clignotants et 3 pour hasard \n");
       scanf("%d", &choix);
    }
-   if(choix==1)
-      init_canon(matrice);
-   if (choix==2)
-      init_clignotants(matrice);
-   if (choix==3)
-      init_random(matrice);
-
+   
+   return choix;
 }
+
 
 
