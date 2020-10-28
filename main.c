dfc4a2e952e2b9ddd2b5fe067e72e0faa054675e
Whozghiar <theolomege@hotmail.fr>
2020-10-28 15:38:21 +0100

    Documentation main.c (choix_init) + Compte-Rendu

--- main.c
+++ main.c
@@ -11,9 +11,13 @@ void affichage(int matrice[][TailleMatrice]);
 int compte_voisin(int matrice[][TailleMatrice], int L, int C);
 void cycle(int matrice[][TailleMatrice]);
 
+/** 
+ * La fonction choix_init renvoie la variable choix, un entier, qui est défini par la saisie de l'utilisateur. 
+*/
+
 int choix_init() {
-   int choix = 0;
-   while((choix<1)||(choix>3)){
+   int choix = 0; // On initialise la variable choix comme un entier initialisé à 0.
+   while((choix<1)||(choix>3)){ // Tant que le choix n'est pas correct, on propose à l'utilisateur d'entrer le type d'initialisation que l'on souhaite.
       printf("Choisissez un type d'initialisation (1 pour planeur, 2 pour clignotants et 3 pour hasard \n");
       scanf("%d", &choix);
    }
