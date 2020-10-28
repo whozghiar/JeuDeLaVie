ce4c34602e1b00f31de99502fa5e80c43f9ce50f
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2020-10-28 14:24:48 +0100

    Affichage : Plus de grille

--- affichage.c
+++ affichage.c
@@ -2,23 +2,21 @@
 #include <stdlib.h>
 
 #define TailleMatrice 10 
-void affiche_ligne(int ligne);
+
 
 void affichage(int matrice[][TailleMatrice]){
     int i,j;
     for (i = 1 ; i < TailleMatrice-1; i++){
-        affiche_ligne(TailleMatrice-2);
         for (j = 1; j < TailleMatrice-1 ; j++){
             if (matrice[i][j]==1){
-                printf("|V");
+                printf(" V");
             }
             else{
-                printf("|M");
+                printf(" M");
             }
         }
-        printf("|\n");
+        printf(" \n");
     }
-    affiche_ligne(TailleMatrice-2);
 }
 
 
