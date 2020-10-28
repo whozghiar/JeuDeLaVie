df29e3c3195448ea08aa955455aa5074c56b82a8
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2020-10-28 16:20:40 +0100

    up main

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ void cycle(int matrice[][TailleMatrice]);
 int choix_init() {
    int choix = 0; // On initialise la variable choix comme un entier initialisé à 0.
    while((choix<1)||(choix>3)){ // Tant que le choix n'est pas correct, on propose à l'utilisateur d'entrer le type d'initialisation que l'on souhaite.
-      printf("Choisissez un type d'initialisation (1 pour planeur, 2 pour clignotants et 3 pour hasard \n");
+      printf("Choisissez un type d'initialisation (1 pour planeur, 2 pour clignotants et 3 pour hasard) \n");
       scanf("%d", &choix);
    }
    
