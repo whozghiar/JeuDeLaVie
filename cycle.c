b978c479f688906e090298c74066790c8fd7bb1f
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2020-10-28 16:04:42 +0100

    Commentaire cycle

--- cycle.c
+++ cycle.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #define TailleMatrice 10 
 
+/** 
+ * La fonction compte_voisin renvoie la variable resultat, un entier, qui est obtenue en calculant le nombre de voisin d'une cellule. 
+ * 
+ * @param matrice une matrice qui contient des entiers, de taille TailleMatrice
+ * 
+ * Elle fait la mise a jour d'un cycle du jeu. On commence par copier la matrice dans matrice de tye entier "CopieMatrice"
+ * On parcourt ensuite la matrice en calculant les voisins grace a la fonction compte_voisin puis elle vérifie
+ * si la cellule est vivante et possède 2 voisins ou si elle possède 3 voisins, si c'est le cas elle y met 1 (vivante)
+ * sinon elle met 0 (morte). 
+ * 
+ * Cette fonction ne retourne rien. 
+*/
+
 void cycle(int matrice[][TailleMatrice]) {
     
     int i,j, nbVoisin;
